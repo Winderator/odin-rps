d9673b83d3422463b4e3bc537697b302102c7f92
Assimilator <Mutebe@bigpond.com>
2024-09-02 19:06:33 +1000

    Make the entire game

--- javascript.js
+++ javascript.js
@@ -1,1 +1,39 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let selection = Math.random()*3
+    if (selection >= 2){return 'rock'}
+    if (selection >= 1){return 'paper'}
+    return 'scissors'
+}
+
+function getHumanChoice(){
+    let choice = prompt()
+    return choice
+}
+
+function playRound(){
+    let human = getHumanChoice().toLowerCase()
+    let computer = getComputerChoice()
+
+    let winner = determineWinner(human,computer)
+    if (winner == 'human') {humanScore++}
+    if (winner == 'computer') {computerScore++}
+    return winner
+}
+
+function determineWinner(a,b){
+    if (a==b) {return "tie"}
+    if (a=="rock" && b=="scissors" || a=="paper" && b=="rock" || a=="scissors" && b=="paper") {return "human"}
+    return "computer"
+}
+
+function playGame(){
+    for (let i=0; i<5; i++){
+        playRound()
+        console.log(`Human:${humanScore}, Computer:${computerScore}`)
+    }
+}
+
+let humanScore = 0
+let computerScore = 0
+
+playGame()
